2fd9e9ee73223eb58caacc15f43b50134354ccc7
saurabh bagalkar <sbagalka@asu.edu>
2019-01-17 20:54:10 +0000

    add output arg

--- goturn/test/show_tracker_vot.py
+++ goturn/test/show_tracker_vot.py
@@ -18,6 +18,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True, help="Path to the prototxt")
 ap.add_argument("-m", "--model", required=True, help="Path to the model")
 ap.add_argument("-v", "--input", required=True, help="Path to the vot directory")
+ap.add_argument("-o", "--output", required=True, help="Path to the output directory where all evaluated images will be stored")
 ap.add_argument("-g", "--gpuID", required=True, help="gpu to use")
 args = vars(ap.parse_args())
 
@@ -26,5 +27,5 @@ objRegressor = regressor(args['prototxt'], args['model'], args['gpuID'], 1, do_t
 objTracker = tracker(False, logger)  # Currently no idea why this class is needed, eventually we shall figure it out
 objLoaderVot = loader_vot(args['input'], logger)
 videos = objLoaderVot.get_videos()
-objTrackerVis = tracker_manager(videos, objRegressor, objTracker, logger)
+objTrackerVis = tracker_manager(videos, objRegressor, objTracker, logger, args['output'])
 objTrackerVis.trackAll(0, 1)
