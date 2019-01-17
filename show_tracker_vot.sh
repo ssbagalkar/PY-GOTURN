ce5212c0e9409d62004b21fef07263dfa0d7a6d6
saurabh bagalkar <sbagalka@asu.edu>
2019-01-17 20:53:10 +0000

    customize for my local

--- show_tracker_vot.sh
+++ show_tracker_vot.sh
@@ -1,9 +1,11 @@
-DEPLOY_PROTO='./nets/tracker.prototxt'		 
-CAFFE_MODEL='./nets/tracker.caffemodel'		
-TEST_DATA_PATH='/media/nrupatunga/data/datasets/VOT/VOT2014/vot2014/'		
+DEPLOY_PROTO='./nets/tracker.prototxt'
+CAFFE_MODEL='./nets/tracker.caffemodel'
+TEST_DATA_PATH='/home/ubuntu/GOTURN_py/data/images/'
+OUTPUT_DIR='/home/ubuntu/GOTURN_py/data/output_images/'
 
 python -m goturn.test.show_tracker_vot \
 	--p $DEPLOY_PROTO \
 	--m $CAFFE_MODEL \
 	--i $TEST_DATA_PATH \
+    --o $OUTPUT_DIR \
 	--g 0
